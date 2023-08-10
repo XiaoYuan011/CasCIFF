3bff059b83e4945984a23efab16b400c27599c52
XiaoYuan <49193728+XiaoYuan011@users.noreply.github.com>
2023-08-10 23:34:08 +0800

    Add files via upload

---
+++ log/Logger.py
@@ -0,0 +1,14 @@
+import os
+import sys
+class Logger():
+    def __init__(self, filename="log.txt"):
+        self.terminal = sys.stdout
+        self.log = open(filename, "a+")
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+
+    def flush(self):
+        pass
+
